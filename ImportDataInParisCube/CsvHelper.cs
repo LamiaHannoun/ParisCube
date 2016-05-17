d1cf28555b2caeb1c5fca843f37be24353282022
hannoun <lamia.hannoun@live.fr>
2016-05-17 12:35:40 +0200

    Squelette application de parsing de .csv

---
+++ ImportDataInParisCube/CsvHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportDataInParisCube
+{
+    public class CsvHelper
+    {
+        private DataRepository _dataRepository;
+        public CsvHelper()
+        {
+            _dataRepository = new DataRepository();
+        }
+        public void ReadVelibData(string path)
+        {
+            var lines = File.ReadAllLines(path).Select(a => a.Split(';'));
+
+            foreach (var line in lines)
+            {
+
+            }
+        }
+
+        public void ReadRestaurants(string path)
+        {
+
+        }
+
+        public void ReadSubwayStations(string path)
+        {
+
+        }
+    }
+}
---
+++ ImportDataInParisCube/DataRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ImportDataInParisCube
+{
+    public class DataRepository
+    {
+        string connectionString { get; }
+        public DataRepository()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+        }
+
+        public void ImportData()
+        {
+            var connectionString = string.Empty;
+            var commandText = string.Empty;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(commandText, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
